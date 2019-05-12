cec4a01dc1ee99e26e472915ea0cfdb35a6fafaa
Lachlan SH <lachlan.stibbard-hawkes@softwire.com>
2019-05-12 11:46:19 -0700

    Add first failing test

--- Tandem.MessageBoard.Api.Tests/Controllers/MessageControllerTests.cs
+++ Tandem.MessageBoard.Api.Tests/Controllers/MessageControllerTests.cs
@@ -1,8 +1,11 @@
-using System;
 using Xunit;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Collections.Generic;
 
 namespace Tandem.MessageBoard.Api.Tests
 {
@@ -14,18 +17,41 @@ namespace Tandem.MessageBoard.Api.Tests
             _factory = factory;
         }
 
-        [Fact]
-        public async Task Test1()
+        [Theory]
+        [InlineData("userId", "message")]
+        [InlineData("userid", "Message")]
+        [InlineData("Userid", "MESSAGE")]
+        [InlineData("UserId", "mEsSaGe")]
+        [InlineData("USERID", "MeSsAgE")]
+        [InlineData("uSeRiD", "mesSAGE")]
+        public async Task Post_BodyPropertiesShouldBeCaseInsensitive(string userIdPropertyName, string messagePropertyName)
         {
             // Arrange
+            const string UserIdPropertyValue = "simon";
+            const string MessagePropertyValue = "Hello darkness, my old friend, I've come to talk with you again";
+
+            var serialisedObjectToPost = GeneratePostBody(userIdPropertyName, messagePropertyName);
+            var stringContent = new StringContent(serialisedObjectToPost, Encoding.UTF8, "application/json");
+
             var client = _factory.CreateClient();
+            await client.PostAsync("/messages", stringContent);
 
             // Act
-            var response = await client.GetAsync("/api/message");
+            var response = await client.GetAsync("/messages?userId=simon");
             var result = await response.Content.ReadAsStringAsync();
 
             // Assert
-            result.Should().Contain("value");
+            result.Should().Contain("userId");
+            result.Should().Contain("message");
+
+
+            string GeneratePostBody(string userId, string message) {
+                var objectToPost = new Dictionary<string, string>();
+                objectToPost.Add(userId, UserIdPropertyValue);
+                objectToPost.Add(message, MessagePropertyValue);
+                var serialisedObject = JsonConvert.SerializeObject(objectToPost);
+                return serialisedObject;
+            }
         }
     }
 }
