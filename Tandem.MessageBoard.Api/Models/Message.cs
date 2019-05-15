d6407cf36410ec429246d3c8bcbf577d70049569
Lachlan SH <lachlan.stibbard-hawkes@softwire.com>
2019-05-14 19:59:21 -0700

    Add message model

--- Tandem.MessageBoard.Api/Models/Message.cs
+++ Tandem.MessageBoard.Api/Models/Message.cs
@@ -3,8 +3,9 @@ namespace Tandem.MessageBoard.Api.Models
 {
     public class Message
     {
-        public Message()
-        {
-        }
+        public Guid MessageId { get; set; }
+        public string UserId { get; set; }
+        public string Message { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
     }
 }
