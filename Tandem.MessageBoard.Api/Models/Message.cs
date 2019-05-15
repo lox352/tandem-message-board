7e441e5c3e1f92b1a1d9d3c11f64a1ee9d1a0636
Lachlan SH <lachlan.stibbard-hawkes@softwire.com>
2019-05-14 20:00:07 -0700

    Add messagecontent property to message class

--- Tandem.MessageBoard.Api/Models/Message.cs
+++ Tandem.MessageBoard.Api/Models/Message.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace Tandem.MessageBoard.Api.Models
 {
     public class Message
     {
         public Guid MessageId { get; set; }
+
         public string UserId { get; set; }
-        public string Message { get; set; }
+
+        [JsonProperty(PropertyName = "Message")]
+        public string MessageContent { get; set; }
+
         public DateTimeOffset CreatedDate { get; set; }
     }
 }
