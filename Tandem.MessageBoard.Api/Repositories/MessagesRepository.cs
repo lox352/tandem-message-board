ddd4ea401f1defb7109480fbca72a9dbe97df559
Lachlan SH <lachlan.stibbard-hawkes@softwire.com>
2019-05-14 23:08:58 -0700

    Implement message repository saving

--- Tandem.MessageBoard.Api/Repositories/MessagesRepository.cs
+++ Tandem.MessageBoard.Api/Repositories/MessagesRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Tandem.MessageBoard.Api.Models;
 
 namespace Tandem.MessageBoard.Api.Repositories
@@ -10,9 +11,27 @@ namespace Tandem.MessageBoard.Api.Repositories
 
     public class MessageRepository : IMessagesRepository
     {
+        private Dictionary<string, Dictionary<Guid, Message>> _messagesByUserId = new Dictionary<string, Dictionary<Guid, Message>>();
+
         public void SaveMessage(Message message)
         {
-            throw new NotImplementedException();
+            AddMessageByUserId(message);
+        }
+
+        private void AddMessageByUserId(Message message)
+        {
+            if (_messagesByUserId.ContainsKey(message.UserId))
+            {
+                var messagesById = _messagesByUserId[message.UserId];
+                messagesById[message.MessageId] = message;
+            } 
+            else
+            {
+                _messagesByUserId.Add(
+                    message.UserId, 
+                    new Dictionary<Guid, Message>() { { message.MessageId, message } }
+                    );
+            }
         }
     }
 }
